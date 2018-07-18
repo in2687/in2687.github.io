7deaf136ab3994aca2c70cf605f50354a0637aaa
in2687 <in2687@naver.com>
2018-07-18 17:49:12 +0900

    Add files via upload

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,26 +1,72 @@
 <?php
+/*
+  PHP contact form script
+  Copyrights BootstrapMade.com
+*/
 
-echo "<script>alert(\"이렇게 띄우면 되자나\");</script>";
-if(empty($_POST['name'])
-   empty($_POST['email'])
-   empty($_POST['subject'])
-   empty($_POST['message'])
-   !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-   {
-		echo "No arguments Provided!";
-		return false;
-   }
+/***************** Configuration *****************/
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+// Enter your email, where you want to receive the messages.
+$contact_email_to = "in2687@naver.com";
 
-$to = 'in2687@naver.com';
-$eamil_subject = "메일이 도착했습니다 : $name";
-$email_body = "당신의 개인 홈페이지에서 메일이 도착했습니다. \n\n", "이름 : $name\n\n 이메일 : $email_addess\n\n 주제 : $subject\n\n 내용 : \n$message";
-$headers = "From : www.bnkc.kr";
-mail($to, $email_subject, $email_body, $headers);
-return true;
+// Subject prefix
+$contact_subject_prefix = "Contat Form Message: " + $_POST["subject"];
 
+// Name too short error text
+$contact_error_name = "Name is too short or empty!";
+
+// Email invalid error text
+$contact_error_email = "Please enter a valid email!";
+
+// Subject too short error text
+$contact_error_subject = "Subject is too short or empty!";
+
+// Message too short error text
+$contact_error_message = "Too short message! Please enter something.";
+
+/********** Do not edit from the below line ***********/
+
+if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+  die('Sorry Request must be Ajax POST');
+}
+
+if(isset($_POST)) {
+
+  $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+
+  if(strlen($name)<4){
+    die($contact_error_name);
+  }
+
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    die($contact_error_email);
+  }
+
+  if(strlen($message)<3){
+    die($contact_error_subject);
+  }
+
+  if(strlen($message)<3){
+    die($contact_error_message);
+  }
+
+  if(!isset($contact_email_from)) {
+    $contact_email_from = "contactform@bnkc.kr" . @preg_replace('/^www\./','', $_SERVER['SERVER_NAME']);
+  }
+
+  $sendemail = mail($contact_email_to, $contact_subject_prefix . $subject, $message,
+    "From:  $name <$contact_email_from>" . PHP_EOL .
+    "Reply-To: $email" . PHP_EOL .
+    "X-Mailer: PHP/" . phpversion()
+  );
+
+  if( $sendemail ) {
+    echo 'OK';
+  } else {
+    echo 'Could not send mail! Please check your PHP mail configuration.';
+  }
+}
 ?>
